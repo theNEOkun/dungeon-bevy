0709e7b8d1713551437262533c7463a4cc9aa396
theNEOkun <sven2000@student.miun.se>
2022-06-23 19:46:08 +0200

    Removed dijkstra

--- src/map/dijkstra.rs
+++
@@ -1,7 +0,0 @@
-use crate::prelude::*;
-use std::collections::BinaryHeap;
-
-fn find_shortest(map: Map, start: &Position, end: &Position) -> Vec<usize> {
-    let mut start_position = map[start].clone();
-    let mut pq = BinaryHeap::new();
-}
