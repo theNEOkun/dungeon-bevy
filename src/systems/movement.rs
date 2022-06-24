04fbc9c91f0b070bdf3171e545596869451a2b5c
theNEOkun <sven2000@student.miun.se>
2022-06-24 13:50:47 +0200

    Working on making the enemies chase

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -40,3 +40,27 @@ pub fn walking_animation(
         }
     }
 }
+
+fn point_to_index(trans: Transform) -> usize {
+    ((trans.translation.y / SCREEN_WIDTH) + trans.translation.x % SCREEN_WIDTH).round() as usize
+}
+
+pub fn chasing(
+    mut commands: Commands,
+    mut enemies: Query<(Entity, &mut Transform), With<Enemy>>,
+    mut player: Query<(Entity, &mut Transform), With<Player>>,
+    map: Res<MapBuilder>,
+) {
+    for (_, e_position) in enemies.iter() {
+        let (_, target) = player.single_mut();
+
+        let map = &map.map;
+        let dijkstra_map = DijkstraMap::new(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize, &[point_to_index(*target)], map, 1024.0);
+
+        let idx = point_to_index(*e_position);
+
+        if let Some(destination)= DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
+
+        }
+    }
+}
